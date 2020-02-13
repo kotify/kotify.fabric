4d1784233810cba19326e9e643938f8a381996be
Konstantin Alekseev <mail@kalekseev.com>
2020-02-13 17:56:02 +0300

    Improve db.dump

--- src/kotify/fabric/aws.py
+++ src/kotify/fabric/aws.py
@@ -1,3 +1,5 @@
+import datetime
+import os.path
 import pathlib
 
 from ._core import Collection, local, task
@@ -27,9 +29,18 @@ def addkey(c):
 
 @task(name="dump")
 def database_dump(c):
-    local_dump = pathlib.Path(c.get("database", {}).get("local_dump", "dump.db"))
+    local_dump = pathlib.Path(
+        c.get("database", {}).get("local_dump", "dump/dump.db")
+    ).absolute()
+    dump_dir = local_dump.parent
+    dump_name = local_dump.name
+    ts = datetime.datetime.now().strftime("%F-%H:%M:%S")
+    ts_path = dump_dir / ("_" + ts).join(os.path.splitext(dump_name))
+    if not dump_dir.exists():
+        dump_dir.mkdir(parents=True)
     local(f"mssh {mssh_args(c)} -- 'sudo -u {c.server.user} -i -- app-db-dump'")
-    local(f"msftp {mssh_args(c)}:{c.server.home_dir}/{local_dump.name} {local_dump}")
+    local(f"msftp {mssh_args(c)}:{c.server.home_dir}/{dump_name} {ts_path}")
+    local(f"ln -f -s {ts_path.name} {local_dump}")
 
 
 ns = Collection("ssh")
